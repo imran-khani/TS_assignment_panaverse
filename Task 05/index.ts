4b0758550b6ce87406a093b97894a95af382a793
imran khan <imranloveh01@gmail.com>
2023-09-08 06:17:13 +0500

    task  5 & 6 completed

--- Task 05/index.ts
+++ Task 05/index.ts
@@ -1,5 +1,3 @@
-// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces. 
-
 const name3: string = " \t\nAlbert Einstein \t\n";
 console.log(name3);
 console.log(name3.trim());
---
+++ Task 06/index.ts
@@ -0,0 +1,15 @@
+// Addition
+const additionResult: number = 5 + 3;
+console.log(`Addition: 5 + 3 = ${additionResult}`);
+
+// Subtraction
+const subtractionResult: number = 15 - 7;
+console.log(`Subtraction: 15 - 7 = ${subtractionResult}`);
+
+// Multiplication
+const multiplicationResult: number = 4 * 2;
+console.log(`Multiplication: 4 * 2 = ${multiplicationResult}`);
+
+// Division
+const divisionResult: number = 16 / 2;
+console.log(`Division: 16 / 2 = ${divisionResult}`);
