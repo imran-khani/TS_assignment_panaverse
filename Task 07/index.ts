89074d89291da211bbfbcc6a66c9938c4cd2ccbc
imran khan <imranloveh01@gmail.com>
2023-09-08 06:20:10 +0500

    task 07

---
+++ Task 07/index.ts
@@ -0,0 +1,5 @@
+// Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.  
+
+// Solution:
+const favoriteNumber: number = 7;
+console.log(`My favorite number is ${favoriteNumber}.`);
