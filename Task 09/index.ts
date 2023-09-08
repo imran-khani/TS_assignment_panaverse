e8f4fb1685d7a2b4e1f9984390d8cd32247d1d01
imran khan <imranloveh01@gmail.com>
2023-09-08 06:23:34 +0500

    task 09

---
+++ Task 09/index.ts
@@ -0,0 +1,9 @@
+// Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.   
+
+
+// Solution:
+const myFriends: string[]= ["John", "Paul", "George", "Ringo"];
+console.log(myFriends[0]);
+console.log(myFriends[1]);
+console.log(myFriends[2]);
+console.log(myFriends[3]);
