4fdb2202be5745e7546a57f8f1243064a8930233
imran khan <imranloveh01@gmail.com>
2023-09-08 06:25:20 +0500

    task 10

---
+++ Task 10/index.ts
@@ -0,0 +1,8 @@
+// Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+// Solution:
+const myFriends2: string[]= ["John", "Paul", "George", "Ringo"];
+console.log(`Hello, ${myFriends2[0]}!`);
+console.log(`Hello, ${myFriends2[1]}!`);
+console.log(`Hello, ${myFriends2[2]}!`);
+console.log(`Hello, ${myFriends2[3]}!`);
